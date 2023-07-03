eae825f6f034b9919955825fb1a6eb0bc6c38796
nguyen <kaitokuroba507@gmail.com>
2023-07-03 16:39:58 +0700

    viet ten

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Nhap ten :");
+string ten = Console.ReadLine();
+Console.WriteLine("ten la :"+ten);
