d98c6ed3e1bc5419ed8e29ada6819fd0591d40ab
nguyen <kaitokuroba507@gmail.com>
2023-07-04 22:45:07 +0700

    BTh 4/7

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,5 +1,33 @@
-﻿Console.Write("Nhap ten :");
-string ten = Console.ReadLine();
-Console.Write("Nhap tuoi :");
-string tuoi = Console.ReadLine();
-Console.WriteLine("ten la :"+ten + " tuoi :"+tuoi);
+﻿System.Console.WriteLine("Nhap ten sinh vien :");
+    string ten = Console.ReadLine();
+System.Console.WriteLine("Nhap ma sinh vien :");
+    string msv = Console.ReadLine();
+System.Console.WriteLine("Nhap lop sinh vien :");
+    string lop = Console.ReadLine();
+float mon1,mon2,mon3,mon4,mon5;
+
+Console.Write("Nhap diem mon thu nhat: ");
+mon1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Nhap diem mon thu hai: ");
+mon2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Nhap diem mon thu ba: ");
+mon3 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Nhap diem mon thu tu: ");
+mon4 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Nhap diem mon thu nam: ");
+mon5 = Convert.ToInt32(Console.ReadLine());
+
+
+float Average = (mon1+mon2+mon3+mon4+mon5) / 5;
+
+System.Console.WriteLine("Trung binh cong cua cac mon la : {0} cua sinh vien {1} - {2} - {3}",Average,ten,msv,lop);
+
+
+
+
+
+    
